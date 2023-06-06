b7a0aa86ef252e7964632914412c7cd03bd04642
Gabriel Abrahao <57734187+abrahao-gabriel@users.noreply.github.com>
2023-06-06 18:27:34 -0300

    Add files via upload

--- ex.hs
+++ ex.hs
@@ -34,3 +34,76 @@ instance Ord a => Semigroup (Min a) where
 minAll :: (Ord a, Bounded a) => [Min a] -> Min a
 minAll = foldr (<>) (Min maxBound)
 
+-----------------------------------------------------------------
+--7.5
+data Fantasma a = Fantasma
+
+instance Functor Fantasma where
+  fmap _ Fantasma = Fantasma
+
+----------------------------------------------------------------
+  --7.2 honestamente entendi nada
+
+  data Dupla a = Dupla a Int a
+
+instance Functor Dupla where
+  fmap f (Dupla x n y) = Dupla (f x) n (f y)
+
+  ----------------------------------------------------------------
+  --8.1 
+
+  instance Functor Caixa where
+  fmap f (Um a) = Um (f a)
+  fmap f (Dois a b) = Dois (f a) (f b)
+  fmap f (Tres a b c) = Tres (f a) (f b) (f c)
+
+
+  ----------------------------------------------------------------
+  --8.2
+
+  import Control.Monad
+
+data Caixa a = Caixa  a a a deriving Show
+
+mult234 :: Double -> Caixa Double
+mult234 x = (\a -> return (2 * a)) x >>= (\b -> return (3 * b)) >>= (\c -> return (4 * c))
+
+instance Functor Caixa where
+  fmap f (Caixa x y z) = Caixa (f x) (f y) (f z)
+
+instance Applicative Caixa where
+  pure a = Caixa a a a
+  (Caixa f g h) <*> (Caixa x y z) = Caixa (f x) (g y) (h z)
+
+instance Monad Caixa where
+  (Caixa x y z) >>= f = Caixa x' y' z'
+    where
+      Caixa x' _ _ = f x
+      Caixa _ y' _ = f y
+      Caixa _ _ z' = f z
+
+
+----------------------------------------------------------------
+--8.3
+-- Tres 1 2 3 >>= mult234 >>= mult234:
+
+-- Começamos com a expressão Tres 1 2 3, que cria uma caixa com os valores 1, 2 e 3 nas coordenadas.
+-- Em seguida, utilizamos o operador >>= para aplicar a função mult234 a esses valores.
+-- A função mult234 multiplica cada valor por 2, 3 e 4 nas coordenadas correspondentes.
+-- Portanto, o resultado será a caixa com os valores resultantes: Tres 2 6 12.
+-- Dois 2 4 >>= mult234:
+
+-- Começamos com a expressão Dois 2 4, que cria uma caixa com os valores 2 e 4 nas coordenadas.
+-- Em seguida, utilizamos o operador >>= para aplicar a função mult234 a esses valores.
+-- A função mult234 multiplica cada valor por 2, 3 e 4 nas coordenadas correspondentes.
+-- Portanto, o resultado será a caixa com os valores resultantes: Dois 4 12.
+-- :kind Coisa:
+
+-- O comando :kind é usado no GHCi para obter o tipo de uma expressão.
+-- No caso de Coisa, não é possível determinar o tipo sem a definição do tipo Coisa. Portanto, não é possível determinar o valor dessa expressão sem mais informações.
+-- Dois 2 3 >>= \_ -> Dois 7 7:
+
+-- Começamos com a expressão Dois 2 3, que cria uma caixa com os valores 2 e 3 nas coordenadas.
+-- Em seguida, utilizamos o operador >>= para aplicar a função lambda \_ -> Dois 7 7 a esses valores.
+-- A função lambda ignora o valor anterior e retorna a caixa Dois 7 7.
+-- Portanto, o resultado será a caixa Dois 7 7.
